4f58f1ad98a3c72e2b58ffc9c0d8935108f0baec
firstrepo <sagarkharal024@gmail.com>
2025-08-24 21:49:00 +0530

    done2

--- backend/routes/recommendRoutes.js
+++ backend/routes/recommendRoutes.js
@@ -3,7 +3,7 @@ import getRecommendations from "../controllers/getRecommendations.js";
 
 const router = express.Router();
 
-// POST /api/v1/recommendations
+
 router.post("/", getRecommendations);
 
 export default router;
